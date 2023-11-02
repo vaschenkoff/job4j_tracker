1647fda8963a388b12f960c518e5889910f35a13
vaschenkoff <vaschenkoff@mail.ru>
2023-11-02 14:04:37 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,6 +1,15 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+
+    public void show() {
+        System.out.println(this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
 
     public String sound() {
     String voice = "may-may";
@@ -12,5 +21,13 @@ public class Cat {
         Cat sparky = new Cat();
         String say = peppy.sound();
         System.out.println("Peppy says " + say);
+        System.out.println("There is gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There is black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.show();
     }
 }
