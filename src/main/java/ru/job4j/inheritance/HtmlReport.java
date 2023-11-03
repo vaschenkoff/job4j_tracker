11bd32396e7591ebe47aa67ad00feae5c78535cc
Andrew Vaschenkoff <vaschenkoff@mail.ru>
2023-11-03 16:55:52 +0300

    7. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/HtmlReport.java
+++ src/main/java/ru/job4j/inheritance/HtmlReport.java
@@ -3,6 +3,7 @@ package ru.job4j.inheritance;
 import org.w3c.dom.Text;
 
 public class HtmlReport extends TextReport {
+    @Override
     public String generate(String name, String body) {
         return "<h1>" + name + "</h1>"
                 + "<br/>"
