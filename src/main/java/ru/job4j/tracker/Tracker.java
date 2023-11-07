6012fb09a6f2609eb260519d72868510c6f53903
Andrew Vaschenkoff <vaschenkoff@mail.ru>
2023-11-07 13:48:42 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,31 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] result = new Item[size];
+        int len = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
+            if (item != null) {
+                result[len] = item;
+                len++;
+            }
+        }
+        return Arrays.copyOf(result, len);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int len = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
+            if (key.equals(item.getName())) {
+                result[len++] = item;
+            }
+        }
+        return Arrays.copyOf(result, len);
     }
 
     public Item findById(int id) {
