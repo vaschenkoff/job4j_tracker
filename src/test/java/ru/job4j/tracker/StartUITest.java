8e05c04707a7ccb2d0455a0ddefff0d2bde2be3b
Andrew Vaschenkoff <vaschenkoff@mail.ru>
2023-11-10 13:04:41 +0300

    9.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -26,4 +26,28 @@ class StartUITest {
         Item expected = new Item("Testing555");
         assertThat(created.getName()).isEqualTo(expected.getName());
     }
+
+    @Test
+    void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "edited item"
+        };
+        StartUI.replaceItem(new MockInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName()).isEqualTo("edited item");
+    }
+
+    @Test
+    void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test535");
+        tracker.add(item);
+        String[] answers = { String.valueOf(item.getId()) };
+        StartUI.deleteItem(new MockInput(answers), tracker);
+        assertThat(tracker.findById(item.getId())).isEqualTo(null);
+    }
 }
