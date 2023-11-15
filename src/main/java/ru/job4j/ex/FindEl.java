d6c39c8106924e12bbf7873e14345e85edb3bcf6
vaschenkoff <vaschenkoff@mail.ru>
2023-11-15 13:10:38 +0300

    0.4. Пользовательские исключения. [#219367 #459840]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -8,9 +8,9 @@ public class FindEl {
                 rsl = i;
                 break;
             }
-            if (rsl == -1) {
-                throw new ElementNotFoundException("Index is not found");
-            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Index is not found");
         }
         return rsl;
     }
@@ -33,7 +33,7 @@ public class FindEl {
     public static void main(String[] args) throws ElementNotFoundException {
         String[] arr = {"privet", "kak dela"};
         try {
-            indexOf(arr, "priv");
+            indexOf(arr, "privet");
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
