9347d52bdcd8df4ab6f0dbcde8b11e27525e1589
Andrew Vaschenkoff <vaschenkoff@mail.ru>
2023-11-24 13:06:47 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String origins : origin) {
+            check.add(origins);
+        }
+        System.out.println(check);
+        for (String checks : text) {
+            if (!check.contains(checks)) {
+                return !rsl;
+            }
+        }
+        return rsl;
+    }
+}
