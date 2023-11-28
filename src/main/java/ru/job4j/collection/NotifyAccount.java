a9b19eb9e0d9bb4d98d7d6c818a3bfff125089ee
vaschenkoff <vaschenkoff@mail.ru>
2023-11-28 14:02:13 +0300

    3. Модели данных и HashSet. [#218530 #462196]

--- src/main/java/ru/job4j/collection/NotifyAccount.java
+++ src/main/java/ru/job4j/collection/NotifyAccount.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class NotifyAccount {
     public static HashSet<Account> sent(List<Account> accounts) {
         HashSet<Account> rsl = new HashSet<>();
-        for (Account result : accounts) {
-            rsl.add(result);
+        for (Account account : accounts) {
+            rsl.add(account);
         }
         return rsl;
     }
