98e0581631c66d2d80d2f60866b6d2d7d9b6271a
vaschenkoff <vaschenkoff@mail.ru>
2023-11-29 11:04:21 +0300

    9. Изменить программу Tracker из 2-го модуля [#10039 #462175]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -47,7 +47,7 @@ public class Tracker {
         int rsl = -1;
         for (int i = 0; i < items.size(); i++) {
             if (items.get(i).getId() == id) {
-                rsl = items.indexOf(items.get(i));
+                rsl = i;
                 break;
             }
         }
