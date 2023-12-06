7222140de9a9419533dbde34c26afb99a1ec10b9
Andrew Vaschenkoff <vaschenkoff@mail.ru>
2023-12-06 16:57:42 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("vaschenkoff@mail.ru", "Andrey Ilich Vaschenkov");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + " " + value);
+        }
+    }
+}
