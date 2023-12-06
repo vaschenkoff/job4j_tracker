2e5378f8dc39eb89512c2eb4a8511b8fc808277b
Andrew Vaschenkoff <vaschenkoff@mail.ru>
2023-12-06 13:47:58 +0300

    1. Интерфейс Queue [#504942]

---
+++ src/main/java/ru/job4j/queue/AppleStore.java
@@ -0,0 +1,30 @@
+package ru.job4j.queue;
+
+import java.util.Queue;
+
+public class AppleStore {
+    private final Queue<Customer> queue;
+
+    private final int count;
+
+    public AppleStore(Queue<Customer> queue, int count) {
+        this.queue = queue;
+        this.count = count;
+    }
+
+    public String getLastHappyCustomer() {
+        for (int i = 0; i < count - 1; i++) {
+            queue.poll();
+        }
+
+        return queue.peek().name();
+    }
+
+    public String getFirstUpsetCustomer() {
+        for (int i = 0; i < count; i++) {
+            queue.poll();
+        }
+
+        return queue.peek().name();
+    }
+}
