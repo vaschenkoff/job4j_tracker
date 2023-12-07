ba46b807911900db22cc46af353b151dfb2e5554
Andrew Vaschenkoff <vaschenkoff@mail.ru>
2023-12-07 13:50:59 +0300

    0. Map, HashMap [#212652 #464853]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("vaschenkoff@mail.ru", "Andrey Ilich Vaschenkov");
+        map.put("vaschenkoff@mail.ru", "Pavel Sergeevich Fedorov");
+        map.put("pushkin@mail.ru", "Alexander Sergeevich Pushkin");
+        map.put("gogol@mail.ru", "Nikolay Vasilevich Gogol");
         for (Map.Entry<String, String> entry : map.entrySet()) {
             String key = entry.getKey();
             String value = entry.getValue();
