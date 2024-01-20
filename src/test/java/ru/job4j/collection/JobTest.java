e0d96be9e211a7b627af88011a6c8ef574dddf5d
vaschenkoff <vaschenkoff@mail.ru>
2024-01-20 12:09:39 +0300

    2. Комбинированный компаратор [#10036 #474926]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -69,9 +69,9 @@ public class JobTest {
     public void whenCompatorByNameAndPrority2() {
         Comparator<Job> cmpNamePriority = new JobAscByName().thenComparing(new JobAscByPriority());
         int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
+                new Job("Fix bug", 0),
                 new Job("Fix bug", 1)
         );
-        assertThat(rsl).isGreaterThan(0);
+        assertThat(rsl).isLessThan(0);
     }
 }
